4324691065f8699572709ef5c944f244cafc87b6
vildan03 <vildankaraduman03@gmail.com>
2026-05-30 22:50:10 +0200

    pipe adds

---
+++ src/executor/exec_pipe/pipe.c
@@ -0,0 +1,88 @@
+
+#include "../../../inc/minishell.h"
+#include "../../../inc/executor.h"
+static void	exit_pipe_error(int fd[2], t_shell *shell)
+{
+	perror("dup2");
+	close(fd[0]);
+	close(fd[1]);
+	cleanup_shell(shell);
+	exit(1);
+}
+
+static void	setup_pipe_child(t_ast_node *node, t_shell *shell,
+		int fd[2], int is_left)
+{
+	int	status;
+
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	if (is_left)
+	{
+		close(fd[0]);
+		if (dup2(fd[1], STDOUT_FILENO) == -1)
+			exit_pipe_error(fd, shell);
+		close(fd[1]);
+		status = exec_ast(node->left, shell);
+	}
+	else
+	{
+		close(fd[1]);
+		if (dup2(fd[0], STDIN_FILENO) == -1)
+			exit_pipe_error(fd, shell);
+		close(fd[0]);
+		status = exec_ast(node->right, shell);
+	}
+	cleanup_shell(shell);
+	exit(status);
+}
+
+static int	fork_pipe_child(t_ast_node *node, t_shell *shell,
+		int fd[2], int is_left)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		close(fd[0]);
+		close(fd[1]);
+		perror("fork");
+		return (-1);
+	}
+	if (pid == 0)
+		setup_pipe_child(node, shell, fd, is_left);
+	return (pid);
+}
+
+static int	wait_pipe_children(pid_t left_pid, pid_t right_pid)
+{
+	int	status;
+
+	waitpid(left_pid, NULL, 0);
+	if (waitpid(right_pid, &status, 0) == -1)
+		return (perror("waitpid"), 1);		
+	return (get_child_status(status));
+}
+
+int	exec_pipe(t_ast_node *node, t_shell *shell)
+{
+	int		fd[2];
+	pid_t	left_pid;
+	pid_t	right_pid;
+
+	if (pipe(fd) == -1)
+		return (perror("pipe"), 1);
+	left_pid = fork_pipe_child(node, shell, fd, 1);
+	if (left_pid == -1)
+		return (1);
+	right_pid = fork_pipe_child(node, shell, fd, 0);
+	if (right_pid == -1)
+	{
+		waitpid(left_pid, NULL, 0);
+		return (1);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	return (wait_pipe_children(left_pid, right_pid));
+}
--- src/executor/execute_nods.c
+++ src/executor/execute_nods.c
@@ -4,9 +4,9 @@
 int	exec_command(t_ast_node *node, t_shell *shell)
 {
 	if (!node)
-		return (1); // think about it well
-	if(!node->args || !node->args[0])
-		return(0);
+		return (1);
+	if (!node->args || !node->args[0])
+		return (0);
 	if (is_builtin(node->args[0]))
 		return (exec_builtin(node, shell));
 	return (exec_external(node, shell));
@@ -25,14 +25,6 @@ int	exec_or(t_ast_node *node, t_shell *shell)
 	(void)shell;
 	return (0);
 }
-
-int	exec_pipe(t_ast_node *node, t_shell *shell)
-{
-	(void)node;
-	(void)shell;
-	return (0);
-}
-
 int	exec_subshell(t_ast_node *node, t_shell *shell)
 {
 	(void)node;
