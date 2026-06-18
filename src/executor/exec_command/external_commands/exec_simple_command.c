54be8ff051342a4f2cb00ea2d3c69182d38ab5ce
kerlee <kerlee@f0r3s20.paris.42.school>
2026-06-18 17:29:53 +0200

    fix: test 29

--- src/executor/exec_command/external_commands/exec_simple_command.c
+++ src/executor/exec_command/external_commands/exec_simple_command.c
@@ -58,6 +58,8 @@ int	exec_simple_command(t_ast_node *node, t_shell *shell)
 
 	if (!node || !node->args || !node->args[0])
 		return (0);
+	if(ft_strncmp(node->args[0], "!", 2) == 0 && node->args[1] == NULL)
+		return(shell->last_exit_status = 1, 1);
 	ast_root = shell->ast_root;
 	token_list = shell->token_list;
 	path = find_command_path(node->args[0], shell->env);
