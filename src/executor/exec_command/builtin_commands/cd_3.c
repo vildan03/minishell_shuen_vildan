2b4e5137c866e91224462f99de918259f3d03a78
vildan03 <vildankaraduman03@gmail.com>
2026-06-24 19:00:32 +0200

    cd splitted

---
+++ src/executor/exec_command/builtin_commands/cd_3.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd_3.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vikaradu <vikaradu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/24 14:01:00 by vikaradu          #+#    #+#             */
+/*   Updated: 2026/06/24 14:01:00 by vikaradu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "executor.h"
+#include "minishell.h"
+
+static void	free_split(char **parts)
+{
+	int	i;
+
+	i = 0;
+	if (!parts)
+		return ;
+	while (parts[i])
+	{
+		free(parts[i]);
+		i++;
+	}
+	free(parts);
+}
+
+static char	**build_logical_stack(char **parts, int *top)
+{
+	char	**stack;
+	int		i;
+
+	i = 0;
+	while (parts[i])
+		i++;
+	stack = malloc(sizeof(char *) * (i + 1));
+	if (!stack)
+		return (NULL);
+	i = 0;
+	*top = 0;
+	while (parts[i])
+	{
+		if (ft_strcmp(parts[i], ".") == 0)
+			;
+		else if (ft_strcmp(parts[i], "..") == 0 && *top > 0)
+			(*top)--;
+		else if (ft_strcmp(parts[i], "..") != 0)
+			stack[(*top)++] = parts[i];
+		i++;
+	}
+	return (stack);
+}
+
+static char	*build_logical_path(char **stack, int top)
+{
+	char	*res;
+	char	*tmp;
+	int		i;
+
+	if (top == 0)
+		return (ft_strdup("/"));
+	res = ft_strdup("");
+	i = 0;
+	while (i < top)
+	{
+		tmp = ft_strjoin(res, "/");
+		free(res);
+		res = ft_strjoin(tmp, stack[i]);
+		free(tmp);
+		i++;
+	}
+	return (res);
+}
+
+static char	*join_target_path(char *pwd, char *target)
+{
+	char	*path;
+	char	*tmp;
+
+	if (target[0] == '/')
+		return (ft_strdup(target));
+	tmp = ft_strjoin(pwd, "/");
+	if (!tmp)
+		return (NULL);
+	path = ft_strjoin(tmp, target);
+	free(tmp);
+	return (path);
+}
+
+char	*resolve_logical(char *pwd, char *target)
+{
+	char	*path;
+	char	**parts;
+	char	**stack;
+	char	*res;
+	int		top;
+
+	if (!pwd || !target)
+		return (NULL);
+	path = join_target_path(pwd, target);
+	if (!path)
+		return (NULL);
+	parts = ft_split(path, '/');
+	free(path);
+	if (!parts)
+		return (NULL);
+	stack = build_logical_stack(parts, &top);
+	if (!stack)
+		return (free_split(parts), NULL);
+	res = build_logical_path(stack, top);
+	return (free(stack), free_split(parts), res);
+}
---
+++ src/executor/exec_command/builtin_commands/cd_4.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd_4.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vikaradu <vikaradu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/24 14:01:00 by vikaradu          #+#    #+#             */
+/*   Updated: 2026/06/24 14:01:00 by vikaradu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "executor.h"
+#include "minishell.h"
+
+char	*resolve_logical(char *pwd, char *target);
+
+static int	logical_matches_dot(char *logical)
+{
+	struct stat	st_log;
+	struct stat	st_dot;
+
+	if (!logical)
+		return (0);
+	if (stat(logical, &st_log) != 0 || stat(".", &st_dot) != 0)
+		return (0);
+	return (st_log.st_dev == st_dot.st_dev
+		&& st_log.st_ino == st_dot.st_ino);
+}
+
+char	*get_new_pwd(char *target, t_shell *shell)
+{
+	char	*new_pwd;
+	char	*pwd;
+	char	*logical;
+
+	pwd = NULL;
+	if (shell && shell->env)
+		pwd = get_env_value_executor(shell->env, "PWD");
+	logical = resolve_logical(pwd, target);
+	if (logical && logical_matches_dot(logical))
+		return (logical);
+	free(logical);
+	new_pwd = getcwd(NULL, 0);
+	if (new_pwd && ft_strcmp(target, "//") == 0 && ft_strcmp(new_pwd, "/") == 0)
+		return (free(new_pwd), ft_strdup("//"));
+	return (new_pwd);
+}
